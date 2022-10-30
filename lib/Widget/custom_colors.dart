582462319ce4a9d3b0ba9befe2b90c64f9b8d66f
smahbamomen <smahbamomen@gmail.com>
2022-10-29 19:59:23 -0700

    chart and home screen

--- lib/Widget/custom_colors.dart
+++ lib/Widget/custom_colors.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class CustomColors{
   static const Color colorYellow = Color(0xFFDFC184);
   static const Color colorWhite = Color(0xFFFFFFFF);
-  static  List<Color> colorWhit = [Color(0xFF274257), Color(0xFF1F1E24)];
+  static  List<Color> colorGradient = [Color(0xFF274257), Color(0xFF1F1E24)];
+  static  const Color colorBlue = Color(0xFF274257);
+  static  const Color colorDarkBlue = Color(0xFF1F1E24);
+  static  const Color colorLightBlue = Color(0xFF3A4F61);
+  static  const Color colorGrey = Color(0xFF747D86);
+  static  const Color colorLightGrey = Color(0xFF4D6274);
+
+
+
 
 }
